d33a4c5ba772ece8b2774cce1ada6adcd8e0dd25
virtouso <s_moeen_kamali@yahoo.com>
2024-09-22 19:18:40 +0330

    ui is ready. adding whatsapp mechanics

--- ClientApp/main.go
+++ ClientApp/main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"fmt"
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
+
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
-	"image"
-	"image/color"
-
-	"log"
-	"os"
-
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"image"
+	"image/color"
+	_ "image/jpeg" // Support for JPEG images
+	"log"
+	"os"
 )
 
 func main() {
@@ -36,6 +37,13 @@ func run(window *app.Window) error {
 	var button widget.Clickable
 	var exitButton widget.Clickable
 	var textBox widget.Editor
+	var img image.Image
+
+	f, err := os.Open("D://x.jpg")
+	if err == nil {
+		defer f.Close()
+		img, _, _ = image.Decode(f)
+	}
 
 	for {
 		switch e := window.Event().(type) {
@@ -44,12 +52,12 @@ func run(window *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 
-			// Check if the button was clicked
 			if button.Clicked(gtx) {
 				fmt.Println("Button clicked!")
 			}
 			if exitButton.Clicked(gtx) {
 				fmt.Println("exit Button clicked!")
+				os.Exit(0)
 			}
 			layout.Flex{
 				Axis:      layout.Vertical,
@@ -57,14 +65,34 @@ func run(window *app.Window) error {
 				Alignment: 2,
 			}.Layout(gtx,
 
-				// Image (or colored box) above the button
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
-						size := gtx.Constraints.Max
-						size.Y = 100
-						size.X = 100
-						rect := image.Rect(0, 0, size.X, size.Y)
-						paint.FillShape(gtx.Ops, color.NRGBA{R: 0, G: 100, B: 200, A: 255}, clip.Rect(rect).Op())
+						size := image.Point{X: 200, Y: 200} // Box size (100x100)
+
+						if img != nil {
+							// Get the image size
+							imgBounds := img.Bounds().Size()
+
+							// Calculate scaling factors
+							scaleX := float32(size.X) / float32(imgBounds.X)
+							scaleY := float32(size.Y) / float32(imgBounds.Y)
+
+							// Create an Affine2D transformation for scaling
+							scaleOp := f32.Affine2D{}.Scale(f32.Pt(0, 0), f32.Pt(scaleX, scaleY))
+
+							// Apply the scaling transformation using op.Affine
+							op.Affine(scaleOp).Add(gtx.Ops)
+
+							// Render the scaled image
+							imgOp := paint.NewImageOp(img)
+							imgOp.Add(gtx.Ops)
+							paint.PaintOp{}.Add(gtx.Ops)
+						} else {
+							// Fallback: Draw a blue box if the image couldn't be loaded
+							rect := image.Rect(0, 0, size.X, size.Y)
+							paint.FillShape(gtx.Ops, color.NRGBA{R: 0, G: 100, B: 200, A: 255}, clip.Rect(rect).Op())
+						}
+
 						return layout.Dimensions{Size: size}
 					},
 				),
